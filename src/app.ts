adb5ce8a4ad6d0d4034fd241ad717826e290476a
LuizHLG <luizlelek08@gmail.com>
2024-02-18 22:39:26 -0300

    deploy config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import "reflect-metadata"
-import "express-async-errors"
-import express, { json } from "express";
+import "reflect-metadata";
+import "express-async-errors";
+import cors from "cors";
 import helmet from "helmet";
+import express, { json } from "express";
 import { tasksRouter } from "./routes/tasks.routes";
 import { HandleErrorsMiddlewares } from "./middlewares/handleErrors.middlewares";
 import { categoriesRouter } from "./routes/categories.routes";
@@ -11,6 +12,7 @@ export const app = express();
 
 app.use(json());
 app.use(helmet());
+app.use(cors())
 app.use("/tasks", tasksRouter);
 app.use("/categories", categoriesRouter);
 app.use("/users", usersRouter);
